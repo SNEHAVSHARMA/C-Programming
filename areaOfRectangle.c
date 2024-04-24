a67363759493984ae26e7ab6bf68d7ea0e38bd13
SNEHAVSHARMA <snehavsharma9125@gmail.com>
2024-04-24 09:24:47 +0530

    Started C Programming

---
+++ areaOfRectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,area;
+    l=10;
+    b=30;
+    area=l*b;
+    printf("length: %d\n",l);
+    printf("breadth: %d\n",b);
+    printf("area of the rectangle is %d",area);
+
+
+}
---
+++ printing.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+ int x=10;
+ int y=20;
+ int z=30;
+ printf("x = %d, y = %d, z = %d", x, y, z);
+}
